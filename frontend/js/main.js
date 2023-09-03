344847f408d87eab0cabcd0e36259729d55ab0b7
Barney Dono <sovietspy2@users.noreply.github.com>
2023-09-03 18:42:52 +0000

    dummy REST call from vanilla js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,6 +4,27 @@ const output = document.getElementById("cookies");
 form.addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent form submission
 
+
+
   // Change the text content of the element
   output.textContent = `the cookie is: ${document.cookie}`;
+
+  const options = {};
+
+  fetch(
+    'https://reqres.in/api/users/2',
+    options
+  )
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    const popup = document.getElementById("modal");
+    popup.style.display = "block";
+
+    const img = document.getElementById("myImage");
+
+    img.src = data.data.avatar;
+  })
+  .catch(err => console.error(err))
 });
