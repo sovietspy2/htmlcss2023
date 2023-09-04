f7eae5ad2f565d93c17423639b3c4961d7efccdd
Barney Dono <sovietspy2@users.noreply.github.com>
2023-09-04 13:30:44 +0200

    landing page content moved to home view

--- dominant/js/views/HomeView.js
+++ dominant/js/views/HomeView.js
@@ -7,6 +7,29 @@ export default class extends AbstractView {
     }
 
     async getHtml() {
-        return "<p> NOT DEFAULT content </p>"
+        return `
+        <article>
+          <h2>
+            My first post
+          </h2>
+          <p>
+            Vivamus fermentum semper porta. Nunc diam velit, adipscing ut
+            tristique vitae sagittis vel odio. Maecenas convallis ullamcorper
+            ultricied. Curabitur ornare, ligula semper consectetur sagittis, nisi
+            diam iaculis velit, is fringille sem nunc vet mi.
+          </p>
+        </article>
+
+        <article>
+          <h2>
+            My second news
+          </h2>
+          <p>
+            Vivamus fermentum semper porta. Nunc diam velit, adipscing ut
+            tristique vitae sagittis vel odio. Maecenas convallis ullamcorper
+            ultricied. Curabitur ornare, ligula semper consectetur sagittis, nisi
+            diam iaculis velit, is fringille sem nunc vet mi.
+          </p>
+        </article>`
     }
 }
